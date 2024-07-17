302fac3cb2751623f1756196726aafd8aacfdf18
dim2603 <dim2603@list.ru>
2024-07-17 13:00:16 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### Coin-O
+
+## What Is It?
+
+This is an application for tracking current cryptocurrency rates in real time using CoinStats OpenAPI. It allows you to add your cryptocurrencies to LocalStorage, adjust your portfolio and evaluate your wallet and returns more efficiently. **A very useful app for cold crypto wallets.**
+
+## Other Dependencies
+
+**Ant Design (antd)** - CSS-styles framework
+**react-chartjs-2** - Chart Module in AppContent
---
+++ src/components/layout/AppHeader.jsx
@@ -0,0 +1,121 @@
+import { Button, Layout, Modal, Select, Space, Drawer } from "antd";
+import { useCrypto } from "../../context/crypto-context";
+import { useEffect, useState } from "react";
+import CoinInfoModal from "../CoinInfoModal";
+import AddAssetForm from "../AddAssetForm";
+
+const headerStyle = {
+  width: "100%",
+  textAlign: "center",
+  height: 60,
+  padding: "1rem",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+};
+
+const handleChange = (value) => {
+  console.log(`selected ${value}`);
+};
+// const options = [
+//   {
+//     label: 'China',
+//     value: 'china',
+//     emoji: '🇨🇳',
+//     desc: 'China (中国)',
+//   },
+//   {
+//     label: 'USA',
+//     value: 'usa',
+//     emoji: '🇺🇸',
+//     desc: 'USA (美国)',
+//   },
+//   {
+//     label: 'Japan',
+//     value: 'japan',
+//     emoji: '🇯🇵',
+//     desc: 'Japan (日本)',
+//   },
+//   {
+//     label: 'Korea',
+//     value: 'korea',
+//     emoji: '🇰🇷',
+//     desc: 'Korea (韩国)',
+//   },
+// ];
+
+export default function AppHeader() {
+  const [select, setSelect] = useState(false);
+  const [coin, setCoin] = useState(null);
+  const [modal, setModal] = useState(false);
+  const [drawer, setDrawer] = useState(false);
+
+  const { crypto } = useCrypto();
+
+  useEffect(() => {
+    const keypress = (event) => {
+      if (event.key === "/") {
+        setSelect((prev) => !prev);
+      }
+    };
+    document.addEventListener("keypress", keypress);
+    return () => document.removeEventListener("keypress", keypress);
+  }, []);
+
+  function handleSelect(value) {
+    console.log(`selected coin: ${value}`);
+    setCoin(crypto.find((c) => c.id === value));
+    setModal(true);
+  }
+
+  return (
+    <Layout.Header style={headerStyle}>
+      <Select
+        style={{
+          width: 250,
+        }}
+        open={select}
+        onSelect={handleSelect}
+        onClick={() => setSelect((prev) => !prev)}
+        value='press " / " to open'
+        options={crypto.map((coin) => ({
+          label: coin.name,
+          value: coin.id,
+          icon: coin.icon,
+        }))}
+        optionRender={(option) => (
+          <Space>
+            <img
+              style={{ width: 20 }}
+              src={option.data.icon}
+              alt={option.data.label}
+            />{" "}
+            {option.data.label}
+          </Space>
+        )}
+      />
+      <Button
+        type="primary"
+        onClick={() => {
+          setDrawer(true);
+        }}
+      >
+        Add Asset
+      </Button>
+
+      <Modal open={modal} onCancel={() => setModal(false)} footer={null}>
+        <CoinInfoModal coin={coin} />
+      </Modal>
+
+      <Drawer
+        width={600}
+        title="Add Asset"
+        onClose={() => setDrawer(false)}
+        open={drawer}
+        destroyOnClose
+      >
+        <AddAssetForm onClose={() => setDrawer(false)} />
+      </Drawer>
+    </Layout.Header>
+  );
+}
